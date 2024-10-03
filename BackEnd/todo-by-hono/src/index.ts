51e93c45bfd785ee47a61c696088b7aca1866c6d
abid211051 <apranto41@gmail.com>
2024-10-03 18:53:54 +0600

    todo-hono-complete

--- BackEnd/todo-by-hono/src/index.ts
+++ BackEnd/todo-by-hono/src/index.ts
@@ -3,15 +3,66 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-let todos: any[] = [{
-  "title": "abid"
-}];
+let todos = {
+  "1":{
+    title:"name",
+    status: "todo"
+  },
+  "2":{
+    title:"age",
+    status: "todo"
+  }
+}
 
 app.get('/todos', (c) => {
-  return c.json(todos)
+  let alltodos=[];
+  for (const key in todos) {
+    alltodos.push({id:key, ...todos[key]});
+  }
+  return c.json(alltodos)
 })
 
+app.get('/todos/:id', (c) => {
+  const {id} = c.req.param();
+  if(!todos[id]) return c.json({message:"Not found"});
 
+  return c.json({id, ...todos[id]});
+})
+
+app.post('/todos', async (c)=>{
+  const {title} = await c.req.json();
+  const id = `${new Date().getTime()}`;
+  const newtodo = {
+    title,
+    status:"todo"
+  }
+  todos[id] = newtodo;
+  return c.json({"message": "Todo created",todo:{id, ...newtodo}});
+})
+
+app.put('/todos/:id', async(c)=>{
+  const {id} = c.req.param();
+  if(!todos[id]) return c.json({message:"Id not matched"});
+  const {title, status} = await c.req.json();
+
+  todos[id].title = title || todos[id].title;
+  todos[id].status = status || todos[id].status;
+
+  return c.json({"message": "Todo updated", todo:{id, ...todos[id]}})
+})
+
+app.delete('/todos/:id', (c)=>{
+  const {id} = c.req.param();
+  if(!todos[id]) return c.json({message:"Id not matched"});
+  delete todos[id];
+
+  return c.json({"message": "Todo deleted"})
+})
+
+app.delete('/todos', (c)=>{
+  todos ={};
+  return c.json({"message": "All Todo deleted"})
+})
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
